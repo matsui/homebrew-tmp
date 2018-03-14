99a60aa2adfd55c3534b511a4a7d4c55295485f5
松井 志穂美 <matsui@rolan.co.jp>
2018-03-14 17:02:36 +0900

    initial commit.

---
+++ hello-matsui.rb
@@ -0,0 +1,9 @@
+ class HelloMatsui < Formula
+  desc "print hello matsui"
+  homepage ""
+  url "https://github.com/matsui/hello-matsui/releases/download/v0.0.1/hello-matsui"
+  sha256 "cb707beb9d5a0ad274ffe5a299595d1a68d4dc60bc73577408f8f625ef626307"
+   def install
+    bin.install
+  end
+end
