db3f85576f074b840ee24353d8a44a392a284f4f
松井 志穂美 <matsui@rolan.co.jp>
2018-03-14 17:18:01 +0900

    fix.

--- hello-matsui.rb
+++ hello-matsui.rb
@@ -4,6 +4,6 @@
   url "https://github.com/matsui/hello-matsui/releases/download/v0.0.1/hello-matsui"
   sha256 "cb707beb9d5a0ad274ffe5a299595d1a68d4dc60bc73577408f8f625ef626307"
    def install
-    bin.install
+    bin.install "hello-matsui"
   end
 end
